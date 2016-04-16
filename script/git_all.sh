dd12fd62c6be8fd2238aeee58d8d7e4fcd3e07ed
Lijing Wang <ooofreedom@gmail.com>
2016-04-16 16:33:10 +0800

    update git.sh

--- script/git_all.sh
+++ script/git_all.sh
@@ -3,5 +3,5 @@ do
    nohup ssh lijing@node${k} 'bash -s' < git.sh &
 done
 
-./git.sh
 #   ssh lijing@node5 'bash -s' < git.sh 
+cd /home/lijing/naxos && git stash && git pull && ./waf configure -l info -m S && ./waf
