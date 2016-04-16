1ed14f675131de98bcbbd6c51b218e7dd55ef04e
Lijing Wang <ooofreedom@gmail.com>
2016-04-16 22:13:37 +0800

    add all.sh

---
+++ script/all.sh
@@ -0,0 +1,6 @@
+# run @ node4
+/home/lijing/NDNPaxos/script/start_naxos_3.sh
+/home/lijing/naxos/script/start_naxos_3.sh
+# run @ node6
+ssh -t root@node6 "/home/lijing/NDNPaxos/script/start_naxos_5.sh"
+ssh -t root@node6 "/home/lijing/NDNPaxos/script/start_naxos_5_2.sh"
