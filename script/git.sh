86b3d0e2bbe617fe6f79c0b0f52a7d5fdbe09025
Lijing Wang <ooofreedom@gmail.com>
2016-04-16 22:34:12 +0800

    update git.sh again

--- script/git.sh
+++ script/git.sh
@@ -1,1 +1,2 @@
-cd naxos && git stash && git pull && ./waf configure -l info -m S && ./waf
+sudo rm -r naxos && git clone https://github.com/PhdLoLi/naxos && cd naxos && ./waf configure -l info -m S && ./waf
+#git clone https://github.com/PhdLoLi/naxos && cd naxos && git stash && git pull && ./waf configure -l info -m S && ./waf
